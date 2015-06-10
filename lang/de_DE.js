3f113cf57979268135126258d49f471fc2a5ef24
xamblak <dev@xamblak.de>
2015-06-10 21:59:41 +0200

    Started internationalisation, added english and german translations

---
+++ lang/de_DE.js
@@ -0,0 +1,23 @@
+/* German translation (de-DE) */
+R.registerLocale('de-DE', 
+{ 
+  "title": 'Insulin Bolus Calculator',
+  "subtitle": 'Blutzuckerspiegel und Mahlzeit eingeben',
+  "input_glucose": 'Blutzucker',
+  "input_meal": 'Mahlzeit',
+  "output_bolus": 'Finaler Bolus',
+  "output_sum": 'Korr (%i) + Meal (%i)',
+  "therapysetuptitle": 'Aktive Therapy Einstellung',
+  "therapyname1": 'Morgens (%s-%s)',
+  "therapyname2": 'Mittags (%s-%s)',
+  "therapyname3": 'Abends (%s-%s)',
+  "therapyname4": 'Nachts (%s-%s)',
+  "therapybutton": 'Ziel %i Korr %i Bolus %i',
+  "input_aim": 'Blutzucker Ziel',
+  "input_corr": 'Korr. Faktor',
+  "input_bolus": 'Bolus',
+  "appsetuptitle": 'Software Einstellungen',
+  "donatebutton": 'Spenden',
+  "de-DE": 'Deutsch',
+  "en-GB": 'Englisch'
+});
---
+++ lang/en_GB.js
@@ -0,0 +1,23 @@
+/* English translation (en-GB) */
+R.registerLocale('en-GB', 
+{ 
+  "title": 'Insulin Bolus Calculator',
+  "subtitle": 'Enter glucose level and meal to be eaten',
+  "input_glucose": 'Glucose Level',
+  "input_meal": 'Amount of Meal',
+  "output_bolus": 'Final Bolus',
+  "output_sum": 'Corr (%i) + Meal (%i)',
+  "therapysetuptitle": 'Active Therapy Setting',
+  "therapyname1": 'Morning (%s-%s)',
+  "therapyname2": 'Noontime (%s-%s)',
+  "therapyname3": 'Evening (%s-%s)',
+  "therapyname4": 'Late (%s-%s)',
+  "therapybutton": 'Aim %i Corr %i Bolus %i',
+  "input_aim": 'Glucose Aim',
+  "input_corr": 'Corr. Factor',
+  "input_bolus": 'Bolus',
+  "appsetuptitle": 'Application Setup',
+  "donatebutton": 'Donate',
+  "de-DE": 'German',
+  "en-GB": 'English'
+});
