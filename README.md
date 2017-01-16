7c22a1eed6d2ec64ab804169f0d7cbe229c5a35f
travis <travis@email.com>
2017-01-16 23:08:23 +0000

    CI Deployment to Github Pages (mxklb@27853f0)

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Insulin Bolus Calculator
-[![Build Status](https://travis-ci.org/mxklb/boluscalculator.svg?branch=master)](https://travis-ci.org/mxklb/boluscalculator)
-
-IBC - An Insulin Bolus Calculator - A HTML5 WebApp.
+IBC - An Insulin Bolus Calculator - A HTML5 WebApp. [![Build Status](https://travis-ci.org/mxklb/boluscalculator.svg?branch=master)](https://travis-ci.org/mxklb/boluscalculator)
 
 A simple and transparent calculator app for the intensive/flexible insulin therapy.
 
-This app is deployed using [gh-pages](https://pages.github.com/). Check it out here: [mxklb.github.io/boluscalculator](http://mxklb.github.io/boluscalculator/)
+This app is deployed using [gh-pages](https://pages.github.com/). Check it out here: [insulinboluscalculator.org](http://insulinboluscalculator.org/)
 
 Feel free to simply use it and/or contribute to the develpoment ..
 
@@ -42,7 +40,7 @@ This app is developed for actual versions of firefox and webkit based browsers l
 
 However:
 
-1. Open [mxklb.github.io/boluscalculator](http://mxklb.github.io/boluscalculator/) in a modern web browser
+1. Open [insulinboluscalculator.org](http://insulinboluscalculator.org/) in a modern web browser
 2. Make sure the therapy settings are displayed according to your needs
 3. Enter the actual _Glucose Level_ (soberly measured) --> _Correction_
 4. Enter the _Amount of Meal_ to be eaten --> _Effective Meal_
---
+++ deploy.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Script used by travis to push static side on master to gh-pages branch
+
+# Get repo url (should be https://github.com/user/repo.git)
+url=$(git config remote.origin.url)
+
+# Remove 'https://' prefix and '.git' suffix
+remote=$(echo "${url:8:-4}")
+echo "Deploying to $remote into branch gh-pages"
+
+# Extract user & repo names
+set -- "$remote" 
+IFS="/"; declare -a split=($*) 
+user="${split[1]}"
+repo="${split[2]}"
+
+# Clone master branch from user repo and make a copy + remove git folder
+git clone --quiet "https://$user:${GH_TOKEN}@github.com/$user/$repo.git" --branch=master source
+cd source
+
+# Copy master branch sources
+mkdir ../tmp
+rsync -Ra ./ ../tmp
+
+# Get latest commit ID from master branch
+head=$(git log --format="%h" -n 1)
+
+# Set lists of deployment files
+readarray -t files < deploy.list
+readarray -t rmfiles < clean.list
+
+# Switch to gh-pages + apply changes
+git checkout --quiet gh-pages
+
+# Overwrite master sources
+rsync -a --exclude=.git ../tmp/ ./
+
+# Remove clean.list files
+for file in ${rmfiles[@]}; do 
+  if [ -f $file ]; then
+    rm -f "$file"
+  fi
+done
+
+# Remove major deployment files
+if [[ "$user" != "mxklb" ]] ; then
+  for file in ${files[@]}; do 
+    if [ -f $file ]; then
+      rm -f "$file"
+    fi
+  done
+fi
+
+# Stage all
+git add -A
+
+# Check for changes
+status=$(git status)
+echo "$status";
+
+# Setup travis git user, commit and push changes
+if [[ $status != *"nothing to commit"* ]] ; then
+  git config user.name "travis"
+  git config user.email "travis@email.com" 
+  git commit -m "CI Deployment to Github Pages ($user@$head)"
+  git push --force --quiet "https://${GH_TOKEN}@$remote" gh-pages:gh-pages > /dev/null 2>&1
+fi
